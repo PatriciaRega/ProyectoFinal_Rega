acd920f1790d5d49e8378323f24a80f54ad08ce9
PatriciaRega <patriciarega12@gmail.com>
2024-03-21 11:23:55 -0300

    versión03 - CRUD hasta CRU

--- AppCoder/models.py
+++ AppCoder/models.py
@@ -10,11 +10,14 @@ class Medico(models.Model):
     email = models.EmailField()
     cod_medico = models.CharField(max_length=10)
 
-class Solicitud(models.Model):
+class Solicitud(models.Model):  #idea: cambiar como se hace la solicitud y que se muestre como fecha, farmaco, nombre paciente, codigo paciente, codigo medico
     farmaco = models.CharField(max_length=60)
     cod_paciente = models.CharField(max_length=30)
     cod_medico = models.CharField(max_length=10)
     fecha = models.DateField()
+
+    def __str__(self):
+        return f"{self.fecha} - {self.farmaco} - {self.cod_paciente} - {self.cod_medico}"
 
 class Paciente(models.Model):
     nombre = models.CharField(max_length=30)
--- AppCoder/urls.py
+++ AppCoder/urls.py
@@ -2,11 +2,24 @@ from django.urls import path
 from AppCoder.views import *
 
 urlpatterns = [
+    
+    path("", inicio, name="Home"),
+
+    #URLS CRUD DEL MODELO
+
     path("crear_medicos/", crear_medico, name = 'crear medicos'),
-    path("crear_pacientes/", crear_paciente, name = 'crear pacientes'),
-    path("crear_solicitudes/", crear_solicitud, name = 'crear solicitudes'),
     path("ver_medicos/", ver_medicos, name = 'ver medicos'),
+    path("actualizar_medicos/<medicos>", ver_medicos, name = 'ver medicos'),
+
+
+    path("crear_pacientes/", crear_paciente, name = 'crear pacientes'),
     path("ver_pacientes/", ver_pacientes, name = 'ver pacientes'),
-    path("buscar_solicitudes/", buscar_solicitudes, name = 'buscar solicitudes'),
-    path("", inicio, name="Home")
+
+    path("crear_solicitudes/", crear_solicitud, name = 'crear solicitudes'),
+    path("ver_solicitudes/", ver_solicitudes, name = 'ver solicitudes'),
+    
+    #URLS DE BUSQUEDA
+
+    path("buscar_solicitudes/", buscar_solicitudes, name = 'buscar solicitudes')
+  
     ]
