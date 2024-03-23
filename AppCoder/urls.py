84b085f8c320321f8c760dcb17973e51a8c04d82
PatriciaRega <patriciarega12@gmail.com>
2024-03-22 23:22:12 -0300

    version04 - Pagina avanzada

--- AppCoder/urls.py
+++ AppCoder/urls.py
@@ -3,14 +3,18 @@ from AppCoder.views import *
 
 urlpatterns = [
     
-    path("", inicio, name="Home"),
+    path("", inicio, name="Inicio"),
+
+    path("login/", iniciar_sesion, name="Iniciar sesion"),
+    path("signup/", registrarse, name="Crear usuario"),
+    path("logout/", cerrar_sesion, name="Cerrar sesion"),
 
     #URLS CRUD DEL MODELO
 
     path("crear_medicos/", crear_medico, name = 'crear medicos'),
     path("ver_medicos/", ver_medicos, name = 'ver medicos'),
-    path("actualizar_medicos/<id_medico>", actualizar_medico),
-    path("borrar_medicos/<id_medico>", borrar_medico),
+    path("actualizar_medicos/<id_medico>", actualizar_medico, name = "Editar"),
+    path("borrar_medicos/<id_medico>", borrar_medico, name = "Eliminar"),
 
     path("crear_pacientes/", crear_paciente, name = 'crear pacientes'),
     path("ver_pacientes/", ver_pacientes, name = 'ver pacientes'),
