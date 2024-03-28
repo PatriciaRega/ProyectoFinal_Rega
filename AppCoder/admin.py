740ec3010eff9c4ed52c88e328e72e7b190c6a36
PatriciaRega <patriciarega12@gmail.com>
2024-03-28 16:10:00 -0300

    vesion06 - Version final

--- AppCoder/admin.py
+++ AppCoder/admin.py
@@ -3,4 +3,12 @@ from .models import *
 
 # Register your models here.
 
+admin.site.register(Medico)
+
+admin.site.register(Paciente)
+
+admin.site.register(Solicitud)
+
+admin.site.register(Farmaco)
+
 admin.site.register(Avatar)
